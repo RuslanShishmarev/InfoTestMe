630b9a7aaaebcd459399e5f08f39920edd3c5b26
Ruslan Shishmarev <mr.shruslan@gmail.com>
2021-10-31 00:37:30 +0300

    remove unnecessary usings

--- InfoTestMe.WebAPI/Models/User.cs
+++ InfoTestMe.WebAPI/Models/User.cs
@@ -1,9 +1,6 @@
 ﻿using InfoTestMe.WebAPI.Models.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InfoTestMe.WebAPI.Models
 {
