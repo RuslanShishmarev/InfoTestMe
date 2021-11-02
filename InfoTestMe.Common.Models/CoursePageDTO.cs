2430bd89c2cce8866afaf18ad7eba713b60b7a70
Ilya-And <ilya.and2011@yandex.ru>
2021-11-02 17:34:46 +0300

    Добавлены новые классы DTO. Добавлено свойство Image в класс TestQuestion.

---
+++ InfoTestMe.Common.Models/CoursePageDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoTestMe.Common.Models
+{
+    public class CoursePageDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public byte[] Image { get; set; }
+        public string AudioFileName { get; set; }
+        public byte[] AudioFile { get; set; }
+        public List<CourseBlockDTO> Blocks { get; set; } = new List<CourseBlockDTO>();
+    }
+}
---
+++ InfoTestMe.Common.Models/CourseThemeDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoTestMe.Common.Models
+{
+    public class CourseThemeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<int> Pages { get; set; }
+    }
+}
